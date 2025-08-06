f12b0d81097c7f8b14d8aba2c041f55eda68d2e0
ibrzica1 <168496865+ibrzica1@users.noreply.github.com>
2025-08-06 08:09:41 +0100

    Associative Arrays Exercises

---
+++ Asoc-Arrays-Ex.php
@@ -0,0 +1,11 @@
+
+<?php
+    $automobili = [
+      "marka" => "Renault",
+      "model" => "Scenic",
+      "godiste" => 2010
+    ];
+
+    echo "Prodajem automobil $automobili[marka] $automobili[model] proizveden $automobili[godiste]";
+
+?>
